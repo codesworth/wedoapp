885fa07fde10709e238f16de15a8f5b48b2748b1
codesworth <codezworth@gmail.com>
2018-12-06 21:28:51 +0000

    Cleaned Up Files

---
+++ WE-DO/AuthVC.swift
@@ -0,0 +1,34 @@
+//
+//  AuthVC.swift
+//  WE-DO
+//
+//  Created by Mensah Shadrach on 06/12/2018.
+//  Copyright © 2018 Mensah Shadrach. All rights reserved.
+//
+
+import UIKit
+
+class AuthVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func m( name: inout ()->()){
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
